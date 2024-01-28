c53dd03b428f5650b4044d24328cd8c7376f57a9
JinSu77 <adrien2098@hotmail.fr>
2024-01-28 13:19:59 +0100

    remove fmt

--- services/userService.go
+++ services/userService.go
@@ -3,13 +3,11 @@ package services
 
 import (
 	"GolandProject/models"
-	"fmt"
 )
 
 // GetUserByUsernameAndPassword récupère un utilisateur à partir de la base de données en fonction du nom d'utilisateur et du mot de passe
 func GetUserByUsernameAndPassword(username, password string) (*models.User, error) {
 	var user models.User
-	fmt.Println("usernameCaca: ", username)
 	if err := GetConnection().Where("username = ? AND password = ?", username, password).First(&user).Error; err != nil {
 		return nil, err
 	}
