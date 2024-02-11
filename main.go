5075ccd41c20d911f369f62d94d98b8bc242ed45
Adrien HA <a_ha@hetic.eu>
2024-02-11 19:46:54 +0100

    set cors

--- main.go
+++ main.go
@@ -47,6 +47,20 @@ func main() {
 	// Initialisez le routeur Gin
 	router := gin.Default()
 
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+		
+		c.Next()
+	})
+	
 	// Setup the cookie store for session management
 	store := cookie.NewStore([]byte("secret"))
 	router.Use(sessions.Sessions("mysession", store))
